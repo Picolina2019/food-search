6c79508f9cf5b70d830a6cc71fdfd27ac93e7b7c
 Katia S <katesakowicz@MacBook-Pro-4.local>
2020-04-30 11:40:53 +0200

    Updates

---
+++ precache-manifest.5a21f113d5c16af7fe0591685279ae9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fbb11b20c30a8bc10b91dcbf077d086",
+    "url": "/food-search/index.html"
+  },
+  {
+    "revision": "6df3b0d38162a671ad66",
+    "url": "/food-search/static/css/main.78a65043.chunk.css"
+  },
+  {
+    "revision": "59ed3aa3a7266f417eed",
+    "url": "/food-search/static/js/2.ebf12115.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/food-search/static/js/2.ebf12115.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6df3b0d38162a671ad66",
+    "url": "/food-search/static/js/main.2d704b09.chunk.js"
+  },
+  {
+    "revision": "4facba81472a86d9e79b",
+    "url": "/food-search/static/js/runtime-main.4991050e.js"
+  }
+]);
